2be7c4207c4f8f5450975be05e0ed938448b966b
Ilse Slootweg <islootweg@mlp10.science.ru.nl>
2023-05-25 17:37:27 +0200

    add logs to gitignore

--- train_lstm.sh
+++ train_lstm.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-#SBATCH --partition=cnczshort
-#SBATCH --gres=gpu:4
+#SBATCH --partition=short
+#SBATCH --gres=gpu:3
 #SBATCH --mem=10G
 #SBATCH --cpus-per-task=6
-#SBATCH --time=12:00:00
 #SBATCH --output=./logs/%j_transformer.out
 #SBATCH --error=./logs/%j_transformer.err
 project_dir=/home/islootweg/challenge-2/MLIP_fog
